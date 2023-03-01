0753d8ebd143222ce36d775e3e1d6693945d34a3
EmsiJoseph <119904801+EmsiJoseph@users.noreply.github.com>
2023-03-01 15:35:33 +0800

    Update success.php

--- ci4/app/Views/comments/success.php
+++ ci4/app/Views/comments/success.php
@@ -2,21 +2,22 @@
   <div class="header">
     <div class="container">
       <nav>
-        <a href="home">
+        <a href="/~mcagbanlog/lab3/ci4/public/home#home">
           <img src="img/icon.png" class="logo">
         </a>
         <ul>
-          <li><a href="/lab3/ci4/public/home#home">Home</a></li>
-          <li><a href="/lab3/ci4/public/home#about">About</a></li>
-          <li><a href="/lab3/ci4/public/home#portfolio">Portfolio</a></li>
-          <li><a href="/lab3/ci4/public/home#contact">Contact</a></li>
+          <li><a href="/~mcagbanlog/lab3/ci4/public/home#home">Home</a></li>
+          <li><a href="/~mcagbanlog/lab3/ci4/public/home#about">About</a></li>
+          <li><a href="/~mcagbanlog/lab3/ci4/public/home#portfolio">Portfolio</a></li>
+          <li><a href="/~mcagbanlog/lab3/ci4/public/home#contact">Contact</a></li>
         </ul>
       </nav>
     </div>
 </div>
+  
 <div style="text-align: center;"class="container">
     <h1>Comment posted successfully!</h1>
-    <a href="/lab3/ci4/public/comments">
+    <a href="/~mcagbanlog/lab3/ci4/public/comments">
         <button type="submit" class="btn btn2">Go back to comment section</button>
     </a>
 </div>
