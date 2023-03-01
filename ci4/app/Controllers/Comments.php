653317758873f351a7768aef7569ee08ef76fdb4
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2023-03-01 11:41:23 +0800

    Combined index and create function

---
+++ ci4/app/Controllers/Comments.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\CommentsModel;
+
+class Comments extends BaseController
+{
+    public function index()
+    {
+        $model = model(CommentsModel::class);
+
+        $data = [
+            'comments'  => $model->getComments(),
+            'title' => 'Comment Section',
+        ];
+
+        // return view('templates/header', $data)
+        //     . view('comments/index')
+        //     . view('templates/footer');
+
+    // create function
+            helper('form');
+
+            // Checks whether the form is submitted.
+            if (! $this->request->is('post')) {
+                // The form is not submitted, so returns the form.
+                return view('templates/header', $data)
+                . view('comments/index')
+                . view('templates/footer');
+            }
+    
+            $post = $this->request->getPost(['name', 'email', 'website', 'comment']);
+    
+            // Checks whether the submitted data passed the validation rules.
+            if (! $this->validateData($post, [
+                'name' => 'required|max_length[255]|min_length[3]',
+                'email' => 'required|max_length[255]|min_length[3]',
+                'website' => 'required|max_length[255]|min_length[3]',
+                'comment'  => 'required|max_length[5000]|min_length[1]',
+            ])) {
+                // The validation fails, so returns the form.
+                return view('templates/header', $data)
+                . view('comments/index')
+                . view('templates/footer');
+            }
+
+            $date = date('F j, Y g:i a');
+    
+            $model->save([
+                'name' => $post['name'],
+                'email' => $post['email'],
+                'website' => $post['website'],
+                'comment'  => $post['comment'],
+                'friendly_reg_date' => $date,
+            ]);
+    
+            // return view('templates/header', $data)
+            //     . view('comments/success')
+            //     . view('templates/footer');
+            return view('templates/header', $data)
+            . view('comments/success')
+            . view('templates/footer');
+        }//index
+    }//class
